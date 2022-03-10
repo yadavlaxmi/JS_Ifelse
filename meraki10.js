2e37a26d3e5e3f3e23ab9e0ea766db704daee2d4
Laxmi Yadav <91363406+yadavlaxmi@users.noreply.github.com>
2022-03-10 16:48:42 +0530

    Add files via upload

---
+++ meraki10.js
@@ -0,0 +1,8 @@
+let readlineSync = require("readline-sync");
+var number=readlineSync.questionInt("enter a number==");
+if (number == 54){
+  console.log("valid") 
+}
+else{
+  console.log("not")
+}
